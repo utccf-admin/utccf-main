a9fdce4d7f9e0a035b82873d83436af92f65913a
latexsupremecist <dan.chua@mail.utoronto.ca>
2023-11-17 16:39:19 -0500

    added year vision

--- components/largeGroup/largeGroup.js
+++ components/largeGroup/largeGroup.js
@@ -5,7 +5,7 @@ import Section from "../section/section";
 export default function LargeGroup({ largeGroup }) {
   return (
     <Section title="Large Group" description={largeGroup.description}>
-      <CardSkeleton visionFile="" name="CCF">
+      <CardSkeleton visionFile={largeGroup.visionFile} name="CCF">
         <MeetingInfo meetingInfo={largeGroup.meetingInfo} />
       </CardSkeleton>
     </Section>
